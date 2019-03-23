ae9e00ef92d3c836c32f17c7355c2feda5c4fd94
Alesya <alesyashublina@gmail.com>
2019-03-23 03:34:50 +0300

    advanced-zeros/add:getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, base){
+  var reminder;
+  var ZerosCount = number;
+  var time_var = base;
+
+  for (let i = 2; i <= base; i++) {
+    if (time_var % i === 0) {
+      reminder = 0;
+      while (time_var % i === 0) {
+        time_var = Math.floor(time_var / i);
+        reminder++;
+      };
+
+      let count = 0;
+      let tempNum = number;
+      while (tempNum / i > 0) {
+        count += Math.floor(tempNum / i);
+        tempNum = Math.floor(tempNum / i);
+      };
+      var timeVar = Math.floor(count / reminder);
+      if (ZerosCount > timeVar) {
+        ZerosCount = timeVar;
+      }
+    }
+  }
+  return ZerosCount;
 }
